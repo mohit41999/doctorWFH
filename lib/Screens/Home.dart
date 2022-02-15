052280fc791da64656b672374a0364260ab26bd5
mohit <mohit41999@gmail.com>
2022-02-15 19:47:59 +0530

    home

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -15,6 +15,7 @@ import 'package:doctor/widgets/navigation_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
 final List<String> imgList = [
@@ -153,11 +154,31 @@ class _HomeScreenState extends State<HomeScreen> {
           )))
       .toList();
   TextEditingController _search = TextEditingController();
+  List<Placemark> address = [];
+  late Position position;
+  Future getcity() async {
+    try {
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+        position.latitude,
+        position.longitude,
+      );
+      print(placemarks);
+      setState(() {
+        address = placemarks;
+      });
+    } catch (err) {}
+  }
 
   @override
   void initState() {
     // TODO: implement initState
-
+    determinePosition().then((value) {
+      setState(() {
+        position = value;
+        getcity();
+        print(position);
+      });
+    });
     super.initState();
   }
 
@@ -167,7 +188,7 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
           centerTitle: true,
           title: commonAppBarTitle(),
-          backgroundColor: appAppBarColor,
+          backgroundColor: Colors.white,
           elevation: 0,
           leading: Builder(
             builder: (context) => commonAppBarLeading(
@@ -183,6 +204,27 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Container(
+              width: double.infinity,
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: commonBtn(
+                    height: 46,
+                    textSize: 12,
+                    s: (address.length == 0)
+                        ? ''
+                        : address[0].administrativeArea! +
+                            ' ' +
+                            address[0].subAdministrativeArea!,
+                    bgcolor: Colors.white,
+                    borderRadius: 5,
+                    borderColor: appblueColor,
+                    borderWidth: 2,
+                    textColor: apptealColor,
+                    onPressed: () {}),
+              ),
+            ),
             GestureDetector(
               onTap: () {
                 Push(context, SearchScreen());
@@ -344,5 +386,42 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
     );
+  }
+
+  Future<Position> determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
   }
 }
